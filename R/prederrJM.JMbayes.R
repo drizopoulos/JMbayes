26ef376f498af9e13d9f5f17a55587366e3e49ed
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2016-08-20 22:51:11 +0200

    updates prederr functions

--- R/prederrJM.JMbayes.R
+++ R/prederrJM.JMbayes.R
@@ -1,5 +1,4 @@
-prederrJM.JMbayes <-
-function (object, newdata, Tstart, Thoriz, lossFun = c("square", "absolute"), 
+prederrJM.JMbayes <- function (object, newdata, Tstart, Thoriz, lossFun = c("square", "absolute"), 
                                interval = FALSE, idVar = "id", simulate = FALSE, M = 100, ...) {
     if (!inherits(object, "JMbayes"))
         stop("Use only with 'JMbayes' objects.\n")
@@ -45,66 +44,71 @@ function (object, newdata, Tstart, Thoriz, lossFun = c("square", "absolute"),
     idalive <- unique(aliveThoriz[[idVar]])
     iddead <- unique(deadThoriz[[idVar]])
     idcens <- unique(censThoriz[[idVar]])
-    Surv.aliveThoriz <- if (is_counting) {
-        survfitJM(object, newdata = aliveThoriz, idVar = idVar, simulate = simulate, M = M,
-                  survTimes = Thoriz, last.time = rep(Tstart, length(idalive)),
-                  LeftTrunc_var = all.vars(TermsT)[1L])
-    } else {
-        survfitJM(object, newdata = aliveThoriz, idVar = idVar, simulate = simulate, M = M,
-                  survTimes = Thoriz, last.time = rep(Tstart, length(idalive)))
-        
-    }
-    Surv.deadThoriz <- if (is_counting) {
-        survfitJM(object, newdata = deadThoriz, idVar = idVar, simulate = simulate,
-                  survTimes = Thoriz, last.time = rep(Tstart, length(iddead)),
-                  LeftTrunc_var = all.vars(TermsT)[1L])
-    } else {
-        survfitJM(object, newdata = deadThoriz, idVar = idVar, simulate = simulate,
-                  survTimes = Thoriz, last.time = rep(Tstart, length(iddead)))
-    }
-    Surv.aliveThoriz <- sapply(Surv.aliveThoriz$summaries, "[", 2)
-    Surv.deadThoriz <- sapply(Surv.deadThoriz$summaries, "[", 2)
-    if (nrow(censThoriz)) {
-        Surv.censThoriz <- if (is_counting) {
-            survfitJM(object, newdata = censThoriz, idVar = idVar, simulate = simulate, M = M,
-                      survTimes = Thoriz, last.time = rep(Tstart, length(idcens)),
+    prederr <- if (length(unique(Time)) > 5) {
+        Surv.aliveThoriz <- if (is_counting) {
+            survfitJM(object, newdata = aliveThoriz, idVar = idVar, simulate = simulate, M = M,
+                      survTimes = Thoriz, last.time = rep(Tstart, length(idalive)),
                       LeftTrunc_var = all.vars(TermsT)[1L])
         } else {
-            survfitJM(object, newdata = censThoriz, idVar = idVar, simulate = simulate, M = M,
-                      survTimes = Thoriz, last.time = rep(Tstart, length(idcens)))
+            survfitJM(object, newdata = aliveThoriz, idVar = idVar, simulate = simulate, M = M,
+                      survTimes = Thoriz, last.time = rep(Tstart, length(idalive)))
+            
         }
-        tt <- Time[indCens]
-        weights <- if (is_counting) {
-            survfitJM(object, newdata = censThoriz, idVar = idVar, simulate = simulate, M = M,
-                      survTimes = Thoriz, last.time = tt[!duplicated(censThoriz[[idVar]])],
+        Surv.deadThoriz <- if (is_counting) {
+            survfitJM(object, newdata = deadThoriz, idVar = idVar, simulate = simulate,
+                      survTimes = Thoriz, last.time = rep(Tstart, length(iddead)),
                       LeftTrunc_var = all.vars(TermsT)[1L])
         } else {
-            survfitJM(object, newdata = censThoriz, idVar = idVar, simulate = simulate, M = M,
-                      survTimes = Thoriz, last.time = tt[!duplicated(censThoriz[[idVar]])])
+            survfitJM(object, newdata = deadThoriz, idVar = idVar, simulate = simulate,
+                      survTimes = Thoriz, last.time = rep(Tstart, length(iddead)))
         }
-        Surv.censThoriz <- sapply(Surv.censThoriz$summaries, "[", 2)
-        weights <- sapply(weights$summaries, "[", 2)
+        Surv.aliveThoriz <- sapply(Surv.aliveThoriz$summaries, "[", 2)
+        Surv.deadThoriz <- sapply(Surv.deadThoriz$summaries, "[", 2)
+        if (nrow(censThoriz)) {
+            Surv.censThoriz <- if (is_counting) {
+                survfitJM(object, newdata = censThoriz, idVar = idVar, simulate = simulate, M = M,
+                          survTimes = Thoriz, last.time = rep(Tstart, length(idcens)),
+                          LeftTrunc_var = all.vars(TermsT)[1L])
+            } else {
+                survfitJM(object, newdata = censThoriz, idVar = idVar, simulate = simulate, M = M,
+                          survTimes = Thoriz, last.time = rep(Tstart, length(idcens)))
+            }
+            tt <- Time[indCens]
+            weights <- if (is_counting) {
+                survfitJM(object, newdata = censThoriz, idVar = idVar, simulate = simulate, M = M,
+                          survTimes = Thoriz, last.time = tt[!duplicated(censThoriz[[idVar]])],
+                          LeftTrunc_var = all.vars(TermsT)[1L])
+            } else {
+                survfitJM(object, newdata = censThoriz, idVar = idVar, simulate = simulate, M = M,
+                          survTimes = Thoriz, last.time = tt[!duplicated(censThoriz[[idVar]])])
+            }
+            Surv.censThoriz <- sapply(Surv.censThoriz$summaries, "[", 2)
+            weights <- sapply(weights$summaries, "[", 2)
+        } else {
+            Surv.censThoriz <- weights <- NA
+        }
+        if (!interval) {
+            (1/nr) * sum(lossFun(1 - Surv.aliveThoriz), lossFun(0 - Surv.deadThoriz),
+                         weights * lossFun(1 - Surv.censThoriz) + (1 - weights) * lossFun(0 - Surv.censThoriz), 
+                         na.rm = TRUE)
+        } else {
+            TimeCens <- object$y$Time
+            deltaCens <- 1 - object$y$event
+            KMcens <- survfit(Surv(TimeCens, deltaCens) ~ 1)
+            times <- TimeCens[TimeCens > Tstart & TimeCens < Thoriz & !deltaCens]
+            times <- sort(unique(times))
+            k <- as.numeric(table(times))
+            w <- summary(KMcens, times = Tstart)$surv / summary(KMcens, times = times)$surv
+            prederr.times <- sapply(times, 
+                                    function (t) prederrJM(object, newdata, Tstart, t,
+                                                           interval = FALSE, idVar = idVar, simulate = simulate)$prederr)
+            num <- sum(prederr.times * w * k, na.rm = TRUE)
+            den <- sum(w * k, na.rm = TRUE)
+            num / den
+        }
     } else {
-        Surv.censThoriz <- weights <- NA
-    }
-    prederr <- if (!interval) {
-        (1/nr) * sum(lossFun(1 - Surv.aliveThoriz), lossFun(0 - Surv.deadThoriz),
-                     weights * lossFun(1 - Surv.censThoriz) + (1 - weights) * lossFun(0 - Surv.censThoriz), 
-                     na.rm = TRUE)
-    } else {
-        TimeCens <- object$y$Time
-        deltaCens <- 1 - object$y$event
-        KMcens <- survfit(Surv(TimeCens, deltaCens) ~ 1)
-        times <- TimeCens[TimeCens > Tstart & TimeCens < Thoriz & !deltaCens]
-        times <- sort(unique(times))
-        k <- as.numeric(table(times))
-        w <- summary(KMcens, times = Tstart)$surv / summary(KMcens, times = times)$surv
-        prederr.times <- sapply(times, 
-                                function (t) prederrJM(object, newdata, Tstart, t,
-                                                    interval = FALSE, idVar = idVar, simulate = simulate)$prederr)
-        num <- sum(prederr.times * w * k, na.rm = TRUE)
-        den <- sum(w * k, na.rm = TRUE)
-        num / den
+        nr <- NA
+        NA
     }
     out <- list(prederr = prederr, nr = nr, Tstart = Tstart, Thoriz = Thoriz, 
                 interval = interval, classObject = class(object), 
