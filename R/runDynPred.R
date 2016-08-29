b02b0728c51bb9c18c8ec84450dedaec006195df
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2016-08-29 11:45:51 +0200

    update to JSS version

--- R/runDynPred.R
+++ R/runDynPred.R
@@ -1,7 +1,7 @@
 runDynPred <-
 function () {
     if (requireNamespace("shiny", quietly = TRUE)) {
-        shiny::runApp(system.file("Demo", package = "JMbayes"))
+        shiny::runApp(system.file("demo", package = "JMbayes"))
     } else {
         cat("\npackage 'shiny' is not installed...")
     }
