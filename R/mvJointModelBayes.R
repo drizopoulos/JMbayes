dc949e18cb88569c008a922223c9e761cfc0b0b1
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2018-09-07 10:08:21 +0200

    Added control option: equal.strata.knots = FALSE

--- R/mvJointModelBayes.R
+++ R/mvJointModelBayes.R
@@ -12,7 +12,7 @@ mvJointModelBayes <- function (mvglmerObject, survObject, timeVar,
                 lng.in.kn = 15L, ordSpline = 4L, diff = 2L, speed_factor = 0.6,
                 GQsurv = "GaussKronrod", GQsurv.k = 15L, seed = 1L,
                 n_cores = max(1, parallel::detectCores() - 1), update_RE = TRUE,
-                light = FALSE)
+                light = FALSE, equal.strata.knots = FALSE)
     control <- c(control, list(...))
     namC <- names(con)
     con[(namc <- names(control))] <- control
